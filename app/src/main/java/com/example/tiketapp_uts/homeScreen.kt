aca72538edd083db159d4159db1a51a0d2957dce
iknip26 <ikhwanhaniffirdaus@mail.ugm.ac.id>
2023-10-31 07:24:14 +0700

    UTS

--- app/src/main/java/com/example/tiketapp_uts/homeScreen.kt
+++ app/src/main/java/com/example/tiketapp_uts/homeScreen.kt
@@ -12,11 +12,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.tiketapp_uts.databinding.ActivityHomeScreenBinding
 
 class homeScreen : AppCompatActivity() {
-    private lateinit var recyclerView: RecyclerView
-    private lateinit var movieList: ArrayList<movie>
-    private lateinit var movieAdapter: movieAdapter
     private lateinit var binding: ActivityHomeScreenBinding
-    private lateinit var deskripsi: Array<String>
 
     companion object{
         var names = ""
@@ -29,24 +25,6 @@ class homeScreen : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         replaceFragment(home())
-
-
-//        deskripsi = resources.getStringArray(R.array.list_deskripsi)
-//
-//        binding.recycleView.setHasFixedSize(true)
-//        binding.recycleView.layoutManager = GridLayoutManager(this, 2)
-//
-//        movieList = ArrayList()
-//
-//        movieList.add(movie(R.drawable.img1, "Grand Turismo", "Sports, Action", "Neill Blomkamp", R.drawable.rate4, "4",  deskripsi[0]))
-//        movieList.add(movie(R.drawable.img2, "The Nun 2", "Horror, Thriller", "Michael Chaves", R.drawable.rate5, "5", deskripsi[1]))
-//        movieList.add(movie(R.drawable.img3, "Doraemon Sky Utopia", "Anime, Sci fi", "Takumi Dôyama", R.drawable.rate4, "4", deskripsi[2]))
-//        movieList.add(movie(R.drawable.img4, "Spy X Family", "Anime, Comedy",  "Tatsuya Endo", R.drawable.rate4, "4", deskripsi[3]))
-//        movieList.add(movie(R.drawable.img5, "Loki", "Sci fi, Action",  "Michael Waldron", R.drawable.rate3, "3",  deskripsi[4]))
-//        movieList.add(movie(R.drawable.img6, "Ice Cold", "Documenter, Politics", "Rob Sixsmith", R.drawable.rate5, "5", deskripsi[5]))
-
-//        movieAdapter = movieAdapter(movieList)
-//        binding.recycleView.adapter = movieAdapter
 
         names = intent.getStringExtra(Login.usernames).toString()
         emails = intent.getStringExtra(Login.phones).toString()
